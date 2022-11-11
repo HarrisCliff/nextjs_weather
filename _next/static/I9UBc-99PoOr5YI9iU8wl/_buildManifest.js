e34575f7d1e863aa936eb4535bdc634e2bf3055d
HarrisCliff <78668850+HarrisCliff@users.noreply.github.com>
2022-11-11 21:38:02 +0000

    Deploying to gh-pages from @ HarrisCliff/nextjs_weather@1f752f76c46f89226e3810b08e6845811596d03d 🚀

---
+++ _next/static/I9UBc-99PoOr5YI9iU8wl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-08dbfbfff60990c9.js","static/chunks/993-6cf65fbb6e0ae320.js","static/chunks/pages/index-6a130cfbc7017b5f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
